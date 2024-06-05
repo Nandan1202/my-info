acd029408261038f3e870154d3d19b00cb697826
Nandan1202 <nandusanju2002@gmail.com>
2024-06-05 14:59:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Title of the app
+st.title("My personal App")
+
+# A paragraph of text
+paragraph = """
+My name is Nandan, curreently i am persuing my MBA course in Dairy Science College.
+"""
+
+# Display the paragraph
+st.write(paragraph)
